4df339f9a59ee81beecbf632c10a22a404edafb9
Damian Czarnota <godevdamian@gmail.com>
2020-05-29 13:14:39 +0200

    bigChanges

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 const mainContent=document.querySelector('.mainContent');
 const mainContentIcons=document.querySelector('.mainContent__icons');
+const mainContentHeader=document.querySelector('.mainContent__header');
+const mainForm=document.querySelector('.mainForm');
+const h3Alert=document.querySelector('.h3Alert');
+const home=document.querySelector('#home');
+const favourites=document.querySelector('#favourites');
+const random=document.querySelector('#random');
 //NAV BAR ANIMATION
 const navSlide=()=>{
     const burger=document.querySelector('.burger');
@@ -42,25 +48,90 @@ async function searchIcons(icons,counts){
 }
 ///Looking for ICON,NUMBER OF ICONS
 async function searchIconsValues() {
+    document.querySelector('.mainContent__icons').innerHTML=""
     const iconValue=document.querySelector('.iconValue').value;
     const numberOfIcons=document.querySelector('.numberOfIcons').value;
-    try{ let data= await searchIcons(iconValue,numberOfIcons)
-        console.log(data);
+    const h3=document.querySelector('.h3Alert');
+    if(iconValue==="" || numberOfIcons<=0 || numberOfIcons>100){
+        document.querySelector('.numberOfIcons').style.borderBottom="2px solid red";
+        document.querySelector('.iconValue').style.borderBottom="2px solid red";
+        h3Alert.style.display="block";
+        h3Alert.textContent="Put correct values"
+    }
+    else if(iconValue!="" && numberOfIcons>=1 && numberOfIcons <=100){
+    document.querySelector('.numberOfIcons').style.borderBottom="1px solid black";
+    document.querySelector('.iconValue').style.borderBottom="1px solid black";
+    h3Alert.style.display="none"
+    const data= await searchIcons(iconValue,numberOfIcons)
+    try{
         //Show icons on mainScreen
+        mainContent.style.left="50%";
+        mainContentHeader.innerHTML=`<i class="fas fa-arrow-left wrapperArrow" ><div class="deleteResult"></div></i>Results for: ${iconValue}`;
         for(let i=0; i<=numberOfIcons; i++){
-            document.querySelector('.mainContent__icons').innerHTML+=`<a href="https://www.iconfinder.com/icons/${data.icons[i].icon_id}" class="icon-main"><img src="${data.icons[i].raster_sizes[4].formats[0].preview_url}"></a>`
+            const href=`https://www.iconfinder.com/icons/${data.icons[i].icon_id}/download/png/512`
+            document.querySelector('.mainContent__icons').innerHTML+=`<div class="icon-main"><img src="${data.icons[i].raster_sizes[4].formats[0].preview_url}"><a href="${href}"><i class="fas fa-save"></i></a> <span class="star"><i class="fas fa-star"></span></i></div>`
         }
-        // mainContent.style.left="50%";
     }
     catch(err){
+        mainContentHeader.innerHTML=`<i class="fas fa-arrow-left wrapperArrow" ><div class="deleteResult"></div></i>Results for: ${iconValue}`;
         console.log(err);
     }
 }
+}
+//CLEARING FUNCTION ON HOME
+function showOnlyHome(){
+    mainContent.style.left="-100%"
+    document.querySelector('.mainNav__list').classList.remove('nav-active');
+}
+//DECLARE FUNCTION THAT WILL RETURN RANDOM ICONS
+async function searchRandomIcons(arrs){
+    const arr=arrs
+    const API_SECRET="eZsT5vw3W46ifQCqEehkJrnFOQhRIvE6iaz293KaPvGvddSqnE18pSiEzUcNbLt2";
+    const url=`https://cors-anywhere.herokuapp.com/https://api.iconfinder.com/v4/icons/search?query=${arr}&count=60&premium=0`;
+    try{const connection= await fetch(url,{
+        method: "GET",
+     headers: {
+        "Authorization": `Bearer  ${API_SECRET}`}
+    });
+    const response= await connection.json();
+    return response;
+    }
+    catch(err){
+        console.log("Ooops, Something went Wrong");
+    }
+
+}
+//MAKING RANDOM CONTENT
+async function searchRandomIconsValues(){
+    document.querySelector('.mainContent__icons').innerHTML=""
+    let randomNumber=Math.floor(Math.random()*(9-0)+0);
+    const arr=['arrow','sun','sky','smile','dark','eye','dot','dog','cat','cow']
+    let arrRandom=arr[randomNumber]
+    const data= await searchRandomIcons(arrRandom);
+    console.log(data);
+    try{
+        //Show icons on mainScreen
+        mainContent.style.left="50%";
+        mainContentHeader.innerHTML=`<i class="fas fa-arrow-left wrapperArrow" ><div class="deleteResult"></div></i>Results for: ${arrRandom}`;
+        for(let i=0; i<=60; i++){
+            const href=`https://www.iconfinder.com/icons/${data.icons[i].icon_id}/download/png/512`
+            document.querySelector('.mainContent__icons').innerHTML+=`<div class="icon-main"><img src="${data.icons[i].raster_sizes[4].formats[0].preview_url}"><a href="${href}"><i class="fas fa-save"></i></a> <span class="star"><i class="fas fa-star"></span></i></div>`
+        }
+    }
+    catch(err){
+        mainContentHeader.innerHTML=`<i class="fas fa-arrow-left wrapperArrow" ><div class="deleteResult"></div></i>Results for: ${arrRandom}`;
+        console.log(err);
+    }
+}
+function setLocalStorage(){
+
+}
 ///event Listeners
-document.querySelector('.wrapperArrow').addEventListener('click', (e) =>{
-e.preventDefault();
-mainContent.style.left="-100%";
-})
+random.addEventListener('click', searchRandomIconsValues);
+home.addEventListener('click', showOnlyHome);
+mainContentHeader.addEventListener('click', () =>{ mainContent.style.left="-100%";});
 document.querySelector('.iconValue').addEventListener('keyup', e => e.keyCode===13 && searchIconsValues());
 document.querySelector('.mainForm__button').addEventListener('click', searchIconsValues);
 
+
+// document.querySelector('.mainContent__icons').innerHTML+=`<a href="https://www.iconfinder.com/icons/${data.icons[i].icon_id}" class="icon-main__href"><img src="${data.icons[i].raster_sizes[4].formats[0].preview_url}"></a>`
